404eef33b5c1c7d266194fbc51c3b3958cb179e5
VeeranalaJahnavi <jahnavi@orfus.in>
2026-01-26 08:21:59 +0530

    feat: Create server.ts to initialize and start the application server.

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,13 @@
-const { AppDataSource } = require("./data-source/typeorm.ts");
 const app = require("./app").default || require("./app");
-
-
 const dotenv = require("dotenv");
 dotenv.config({ path: "./src/config/.env" });
+
 async function startServer() {
     try {
-        console.log(AppDataSource);
-        await AppDataSource.initialize();
-        console.log("Database connected")
-        const port = process.env.PORT;
+        const port = process.env.PORT || 3000;
         app.listen(port, () => {
             console.log(`Server is running on port ${port}`);
         });
-
-
     } catch (error) {
         console.log("Failed to start server", error);
         process.exit(1);
